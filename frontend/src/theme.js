8bc0476bee5c5250c1a7b7e97b1aec60004e7cdb
NullDefault <davisha999@gmail.com>
2022-01-05 17:41:53 -0500

    changed theme to joc official colors

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,16 +3,22 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#ffc046",
-      main: "#ff8f00",
-      dark: "#c56000",
+      light: "#ffff51",
+      main: "#ffcd00",
+      dark: "#c79d00",
       contrastText: "#000",
     },
     secondary: {
-      light: "#718792",
-      main: "#455a64",
-      dark: "#1c313a",
+      light: "#979797",
+      main: "#696969",
+      dark: "#3e3e3e",
       contrastText: "#fff",
+    },
+    info: {
+      light: "#5ee1f6",
+      main: "#00afc3",
+      dark: "#007f93",
+      contrastText: "#000",
     },
   },
 });
