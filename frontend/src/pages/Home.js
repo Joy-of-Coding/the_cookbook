88455ad681d4546453692ae01ca6658b5c3c0b5d
NullDefault <davisha999@gmail.com>
2022-01-05 17:53:44 -0500

    added some flair to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,25 @@
+import { Box } from "@mui/material";
+import svg_art from "../assets/coding_art.svg";
+
 export default function Home() {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
       <h1>Home</h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-      tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
-      doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-      quasi quidem quibusdam.
-    </div>
+      <img alt="funky illustration" src={svg_art} />
+      <Box sx={{ width: "50%" }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Box>
+    </Box>
   );
 }
