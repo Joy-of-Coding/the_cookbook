0701a64cac5ab998f6f1e9e9ce1d54f8649f67ee
NullDefault <davisha999@gmail.com>
2022-01-10 16:46:37 -0500

    updated header logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,20 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import HideOnScroll from "./utility/HideOnScroll";
-import joc_logo from "../assets/joc_circle.png";
+import text_logo from "../assets/cookbook_text.png";
 
 export default function Header() {
   return (
     <HideOnScroll>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
-          <Toolbar>
-            <Box sx={{ margin: 2 }}>
+          <Toolbar variant="dense">
+            <Box sx={{ flexGrow: 1 }}>
               <img
-                alt="joy of coding logo"
-                src={joc_logo}
-                style={{ height: "64px" }}
+                alt="cookbook text logo"
+                src={text_logo}
+                style={{ height: "82px" }}
               />
             </Box>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              The Cookboook
-            </Typography>
             <Button color="inherit" href="/login">
               Login
             </Button>
