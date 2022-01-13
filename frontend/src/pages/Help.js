12b21d6dd6a2aa021dbf59343d7c2bc7be9d5f0b
NullDefault <davisha999@gmail.com>
2022-01-13 16:06:08 -0500

    small temp style on help page

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -1,11 +1,23 @@
+import { Box } from "@mui/material";
+
 export default function Help() {
   return (
-    <div>
-      <h1>Help</h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-      tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
-      doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-      quasi quidem quibusdam.
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <Box sx={{ width: "50%", textAlign: "center" }}>
+        <h1>Help</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Box>
+    </Box>
   );
 }
